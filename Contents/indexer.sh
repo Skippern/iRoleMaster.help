c4375eecc3a45eec300056aa641b791d6f4f8452
Skippern <skippern@gimnechiske.org>
2015-08-29 13:50:51 -0300

    Initial files
    Initial files

---
+++ Contents/indexer.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+hiutil -C -a -g -v -s en -l en -f Resources/en.lproj/iRoleMaster.helpindex Resources/
+#hiutil -C -a -g -vvv -s en -l en -f Resources/en.lproj/iRoleMaster.helpindex Resources/
